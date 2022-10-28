88e834125feac15559371afe782efe28ae95ff99
Gautam Adatra <gautamkiran123@gmail.com>
2022-10-28 16:51:39 +0530

    feat: added top navbar component

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TopNav = () => {
+    return(
+        <>
+            <div className="dark-theme top-nav">
+            <div className="brand-heading">
+                GazeTv
+            </div>
+            <button className="btn btn-outline-primary login-button">Login</button>
+            </div>  
+        </>
+    )
+}
+
+export {TopNav};
